0e1d8db7d0c07aadfa5893d72f02fa2bdd0059b6
Marvin Mendes <marvin.m.lopes@gmail.com>
2021-06-01 14:54:18 -0300

    Ajustando annotations para acesso do front-end | documentando a api no swagger.

--- src/main/java/com/bootcamp/apisantanderbootcamp/ApiSantanderBootcampApplication.java
+++ src/main/java/com/bootcamp/apisantanderbootcamp/ApiSantanderBootcampApplication.java
@@ -3,6 +3,7 @@ package com.bootcamp.apisantanderbootcamp;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -15,8 +16,8 @@ public class ApiSantanderBootcampApplication {
     }
 
     @Bean
-    public OpenAPI customOpenApi() {
-        return new OpenAPI().info(new Info().title("")
+    public OpenAPI customOpenApi(@Value("${application.description}") String description ) {
+        return new OpenAPI().info(new Info().title(description)
                 .version("1.0")
                 .termsOfService("http://swagger.io/terms")
                 .license(new License().name("Apache 2.0").url("http://springdoc.org")));
